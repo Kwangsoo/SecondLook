cf61011acf5ce53a495219947614496ae7d96206
Kwangsoo Lee <lee@kwangsoo.com>
2018-09-28 12:08:49 -0700

    End of Section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -15,30 +16,49 @@ import { AlertifyService } from './_services/alertify.service';
 
 // // RECOMMENDED (doesn't work with system.js)
 // import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { MemberListComponent } from './memberList/memberList.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
+
 // // or
 
 // @NgModule({
-//   imports: [BsDropdownModule.forRoot(),...]
+//    imports: [
+//       BsDropdownModule.forRoot(),
+//       ...\\\\n//
+//    ],
+//    declarations: [
+//       MemberListComponent,
+//       ListsComponent,
+//       MessagesComponent
+//    ]
 // })
 // export class AppModule(){}
 
 @NgModule({
    declarations: [
-      AppComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent
+        AppComponent,
+        NavComponent,
+        HomeComponent,
+        RegisterComponent,
+        MemberListComponent,
+        ListsComponent,
+        MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
       ErrorInterceptorProvider,
-      AlertifyService
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
